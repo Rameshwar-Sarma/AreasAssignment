472857e1a00f53450287c556c6a6d62edc71a52a
Rameshwara Sarma <84918440+Rameshwar-Sarma@users.noreply.github.com>
2024-12-22 21:03:15 +0530

    Delete PerformanceTipsAssignment directory

--- PerformanceTipsAssignment/PerformanceTipsAssignment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PerformanceTipsAssignment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
